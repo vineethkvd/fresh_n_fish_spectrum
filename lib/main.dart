c6162e505e45805534efc4d1823cd1d4c6288458
Your Name <spectrumgithubtraining>
2023-12-12 10:44:23 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fresh_n_fish_spectrum/View/auth_ui/sign-up-screen.dart';
 import 'View/auth_ui/sign-in-screen.dart';
-void main(){
-  runApp(MyApp());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
