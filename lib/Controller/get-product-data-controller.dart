57b0caf3f47064aa14e8139708d8b902d354e5b4
Your Name <spectrumgithubtraining>
2023-12-15 17:48:41 +0530

    done

--- lib/Controller/get-product-data-controller.dart
+++ lib/Controller/get-product-data-controller.dart
@@ -4,8 +4,7 @@ import 'package:get/get.dart';
 class GetProductDataController extends GetxController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<List<QueryDocumentSnapshot<Object?>>> getProductData(
-      String uId) async {
+  Future<List<QueryDocumentSnapshot<Object?>>> getProductData() async {
     final QuerySnapshot productData = await _firestore
         .collection('products')
         .where('isSale', isEqualTo: false)
