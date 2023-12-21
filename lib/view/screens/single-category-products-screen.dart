8d61c896ef547df956c24ca0fe43f929af18c31d
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-12-21 22:43:34 +0530

    done

--- lib/view/screens/single-category-products-screen.dart
+++ lib/view/screens/single-category-products-screen.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fresh_n_fish_spectrum/View/main_page.dart';
 import 'package:get/get.dart';
 import 'package:image_card/image_card.dart';
 
@@ -23,99 +25,175 @@ class _AllSingleCategoryProductsScreenState
     extends State<AllSingleCategoryProductsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppConstant.appMainColor,
-        title: Text('Products'),
-      ),
-      body: FutureBuilder(
-        future: FirebaseFirestore.instance
-            .collection('products')
-            .where('categoryId', isEqualTo: widget.categoryId)
-            .get(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text("Error"),
-            );
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Container(
-              height: Get.height / 5,
-              child: Center(
-                child: CupertinoActivityIndicator(),
-              ),
-            );
-          }
-
-          if (snapshot.data!.docs.isEmpty) {
-            return Center(
-              child: Text("No category found!"),
-            );
-          }
-
-          if (snapshot.data != null) {
-            return GridView.builder(
-              itemCount: snapshot.data!.docs.length,
-              shrinkWrap: true,
-              physics: const BouncingScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 3,
-                crossAxisSpacing: 3,
-                childAspectRatio: 1.19,
-              ),
-              itemBuilder: (context, index) {
-                final productData = snapshot.data!.docs[index];
-                ProductModel productModel = ProductModel(
-                  productId: productData['productId'],
-                  categoryId: productData['categoryId'],
-                  productName: productData['productName'],
-                  categoryName: productData['categoryName'],
-                  salePrice: productData['salePrice'],
-                  fullPrice: productData['fullPrice'],
-                  productImages: productData['productImages'],
-                  deliveryTime: productData['deliveryTime'],
-                  isSale: productData['isSale'],
-                  productDescription: productData['productDescription'],
-                  createdAt: productData['createdAt'],
-                  updatedAt: productData['updatedAt'],
-                );
-                
-                return Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () => Get.to(() =>
-                          ProductDetailsScreen(productModel: productModel)),
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Container(
-                          child: FillImageCard(
-                            borderRadius: 20.0,
-                            width: Get.width / 2.3,
-                            heightImage: Get.height / 10,
-                            imageProvider: CachedNetworkImageProvider(
-                              productModel.productImages[0],
-                            ),
-                            title: Center(
-                              child: Text(
-                                productModel.productName,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(fontSize: 12.0),
-                              ),
-                            ),
-                          ),
-                        ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppConstant.appMainColor,
+          elevation: 2,
+          leading: IconButton(
+              onPressed: () => Get.offAll(() => const MainPage(),
+                  transition: Transition.leftToRightWithFade),
+              icon: Icon(CupertinoIcons.back, color: Colors.white)),
+          centerTitle: true,
+          title: Text("Category product screen",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.sp,
+                fontFamily: 'Roboto-Regular',
+              )),
+        ),
+        body: Container(
+          width: Get.width,
+          height: Get.height,
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FutureBuilder(
+                future: FirebaseFirestore.instance
+                    .collection('products')
+                    .where('categoryId', isEqualTo: widget.categoryId)
+                    .get(),
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text("Error"),
+                    );
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Container(
+                      height: Get.height / 5,
+                      child: Center(
+                        child: CupertinoActivityIndicator(),
                       ),
-                    ),
-                  ],
-                );
-              },
-            );
-          }
+                    );
+                  }
 
-          return Container();
-        },
+                  if (snapshot.data!.docs.isEmpty) {
+                    return Center(
+                      child: Text("No category found!"),
+                    );
+                  }
+
+                  if (snapshot.data != null) {
+                    return GridView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      shrinkWrap: true,
+                      physics: const BouncingScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 3,
+                        crossAxisSpacing: 3,
+                        childAspectRatio: 1.19,
+                      ),
+                      itemBuilder: (context, index) {
+                        final productData = snapshot.data!.docs[index];
+                        ProductModel productModel = ProductModel(
+                          productId: productData['productId'],
+                          categoryId: productData['categoryId'],
+                          productName: productData['productName'],
+                          categoryName: productData['categoryName'],
+                          salePrice: productData['salePrice'].toString(),
+                          fullPrice: productData['fullPrice'].toString(),
+                          productImages: productData['productImages'],
+                          deliveryTime: productData['deliveryTime'],
+                          isSale: productData['isSale'],
+                          productDescription: productData['productDescription'],
+                          createdAt: productData['createdAt'],
+                          updatedAt: productData['updatedAt'],
+                        );
+
+                        return Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black26,
+                                width: 2.0.w,
+                              ),
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15.r),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.grey.withOpacity(.5),
+                                    offset: const Offset(3, 2),
+                                    blurRadius: 7.r)
+                              ]),
+                          child: Column(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  Get.off(ProductDetailsScreen(
+                                      productModel: productModel));
+                                },
+                                child: SizedBox(
+                                  width: 150.w,
+                                  height: 150.h,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(13.0),
+                                    child: Image.network(
+                                      productModel.productImages[0],
+                                      width: double.infinity,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Flexible(
+                                child: Text(
+                                  productModel.productName,
+                                  style: TextStyle(
+                                      color: const Color(0xFF505050),
+                                      fontFamily: 'Poppins',
+                                      fontSize: 14.sp,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.h,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding:  EdgeInsets.only(left: 13.0.w),
+                                    child: Text(
+                                      ' ₹ ${productModel.fullPrice}',
+                                      style: TextStyle(
+                                          color: const Color(0xFFCF1919),
+                                          fontFamily: 'Poppins',
+                                          fontSize: 14.sp,
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 30.w,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0.w),
+                                    child: CircleAvatar(
+                                      backgroundColor: const Color(0xFF660018),
+                                      child: IconButton(
+                                          icon: const Icon(Icons.add_shopping_cart,
+                                              color: Colors.white),
+                                          onPressed: () async {
+
+                                          }),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  }
+
+                  return Container();
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
