829cb58e8dab8445cef8eaac8a947242841d0383
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-12-22 12:03:22 +0530

    done

--- lib/view/screens/product-deatils-screen.dart
+++ lib/view/screens/product-deatils-screen.dart
@@ -9,6 +9,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import '../../Models/product-model.dart';
 import '../../Utils/app-constant.dart';
+import '../../controller/get-cart-product-controller.dart';
 import '../../models/cart-model.dart';
 import '../auth_ui/sentopt.dart';
 import '../main_page.dart';
@@ -23,12 +24,13 @@ class ProductDetailsScreen extends StatefulWidget {
 
 class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   User? user = FirebaseAuth.instance.currentUser;
+  final CartItemController _CartItemController = Get.put(CartItemController());
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          iconTheme: IconThemeData(color: AppConstant.appTextColor),
+          iconTheme: const IconThemeData(color: AppConstant.appTextColor),
           backgroundColor: AppConstant.appMainColor,
           centerTitle: true,
           title: Text("Products details page",
@@ -40,7 +42,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
           leading: IconButton(
               onPressed: () => Get.offAll(() => const MainPage(),
                   transition: Transition.leftToRightWithFade),
-              icon: Icon(CupertinoIcons.back, color: Colors.white)),
+              icon: const Icon(CupertinoIcons.back, color: Colors.white)),
         ),
         body: Container(
           alignment: Alignment.center,
@@ -100,13 +102,12 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(
-                                widget.productModel.productName,style: TextStyle(
-                                color: AppConstant.appMainColor,
-                                fontSize: 18.sp,
-                                fontFamily: 'Roboto-Bold',
-                              )
-                              ),
+                              Text(widget.productModel.productName,
+                                  style: TextStyle(
+                                    color: AppConstant.appMainColor,
+                                    fontSize: 18.sp,
+                                    fontFamily: 'Roboto-Bold',
+                                  )),
                               const Icon(Icons.favorite_outline)
                             ],
                           ),
@@ -121,20 +122,21 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                               widget.productModel.isSale == true &&
                                       widget.productModel.salePrice != ''
                                   ? Text(
-                                  "Price : ₹ " + widget.productModel.salePrice,style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15.sp,
-                                fontFamily: 'Roboto-Bold',
-                              )
-
-                                    )
+                                      "Price : ₹ " +
+                                          widget.productModel.salePrice,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15.sp,
+                                        fontFamily: 'Roboto-Bold',
+                                      ))
                                   : Text(
-                                "Price : ₹ " + widget.productModel.fullPrice,style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15.sp,
-                                fontFamily: 'Roboto-Bold',
-                              )
-                                    ),
+                                      "Price : ₹ " +
+                                          widget.productModel.fullPrice,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15.sp,
+                                        fontFamily: 'Roboto-Bold',
+                                      )),
                             ],
                           ),
                         ),
@@ -144,29 +146,24 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                         child: Container(
                           alignment: Alignment.topLeft,
                           child: Text(
-                            "Category : " + widget.productModel.categoryName,
+                              "Category : " + widget.productModel.categoryName,
                               style: TextStyle(
                                 color: Colors.black,
                                 fontSize: 13.sp,
                                 fontFamily: 'Roboto-Regular',
-                              )
-
-                          ),
+                              )),
                         ),
                       ),
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Container(
                           alignment: Alignment.topLeft,
-                          child: Text(
-                            widget.productModel.productDescription,
+                          child: Text(widget.productModel.productDescription,
                               style: TextStyle(
                                 color: Colors.black,
                                 fontSize: 13.sp,
                                 fontFamily: 'Roboto-Regular',
-                              )
-
-                          ),
+                              )),
                         ),
                       ),
                       Padding(
@@ -183,14 +180,12 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                                   borderRadius: BorderRadius.circular(20.0),
                                 ),
                                 child: TextButton(
-                                  child: Text(
-                                    "WhatsApp",
+                                  child: Text("WhatsApp",
                                       style: TextStyle(
                                         color: AppConstant.appTextColor,
                                         fontSize: 13.sp,
                                         fontFamily: 'Roboto-Regular',
-                                      )
-                                  ),
+                                      )),
                                   onPressed: () {
                                     return null;
                                   },
@@ -209,15 +204,17 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                                   borderRadius: BorderRadius.circular(20.0),
                                 ),
                                 child: TextButton(
-                                  child: Text(
+                                  child: const Text(
                                     "Add to cart",
                                     style: TextStyle(
                                         color: AppConstant.appTextColor),
                                   ),
                                   onPressed: () async {
                                     // Get.to(() => SignInScreen());
-
-                                    await checkProductExistence(uId: user!.uid);
+                                    await _CartItemController
+                                        .checkProductExistence(
+                                            uId: user!.uid,
+                                            productModel: widget.productModel);
                                   },
                                 ),
                               ),
@@ -234,64 +231,5 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
         ),
       ),
     );
-  }
-
-  Future<void> checkProductExistence({
-    required String uId,
-    int quantityIncrement = 1,
-  }) async {
-    final DocumentReference documentReference = FirebaseFirestore.instance
-        .collection('cart')
-        .doc(uId)
-        .collection('cartOrders')
-        .doc(widget.productModel.productId.toString());
-
-    DocumentSnapshot snapshot = await documentReference.get();
-
-    if (snapshot.exists) {
-      int currentQuantity = snapshot['productQuantity'];
-      int updatedQuantity = currentQuantity + quantityIncrement;
-      double totalPrice = double.parse(widget.productModel.isSale
-              ? widget.productModel.salePrice
-              : widget.productModel.fullPrice) *
-          updatedQuantity;
-
-      await documentReference.update({
-        'productQuantity': updatedQuantity,
-        'productTotalPrice': totalPrice
-      });
-
-      print("product exists");
-    } else {
-      await FirebaseFirestore.instance.collection('cart').doc(uId).set(
-        {
-          'uId': uId,
-          'createdAt': DateTime.now(),
-        },
-      );
-
-      CartModel cartModel = CartModel(
-        productId: widget.productModel.productId,
-        categoryId: widget.productModel.categoryId,
-        productName: widget.productModel.productName,
-        categoryName: widget.productModel.categoryName,
-        salePrice: widget.productModel.salePrice,
-        fullPrice: widget.productModel.fullPrice,
-        productImages: widget.productModel.productImages,
-        deliveryTime: widget.productModel.deliveryTime,
-        isSale: widget.productModel.isSale,
-        productDescription: widget.productModel.productDescription,
-        createdAt: DateTime.now(),
-        updatedAt: DateTime.now(),
-        productQuantity: 1,
-        productTotalPrice: double.parse(widget.productModel.isSale
-            ? widget.productModel.salePrice
-            : widget.productModel.fullPrice),
-      );
-
-      await documentReference.set(cartModel.toMap());
-
-      print("product added");
-    }
   }
 }
