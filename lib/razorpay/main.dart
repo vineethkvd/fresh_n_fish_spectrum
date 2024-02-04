56adcaae05fa402627c6868ef95beb74934bf1ce
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2024-02-04 18:13:25 +0530

    done

--- lib/razorpay/main.dart
+++ lib/razorpay/main.dart
@@ -1,4 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:fresh_n_fish_spectrum/main.dart';
+import 'package:fresh_n_fish_spectrum/razorpay/razorpay_payment.dart';
 void main(){
+runApp(const MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Razor pay",
+      home: RazorPayPage(),
+    );
+  }
 }
